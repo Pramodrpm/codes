d5c1fc3992b0e5c3723d6b63fb2f38cccc8ff10a
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-02-03 10:47:53 +0530

    Update functions.py
    commit

--- functions.py
+++ functions.py
@@ -5,3 +5,16 @@ def O_func(x):
 
 my_func=O_func(2)
 print(my_func(3))
+
+
+
+
+#output = 6
+
+
+
+#tracing
+
+"""Here, the O_func is a higher-order function that takes a single argument x and returns another function i_func. The returned 
+i_func is a closure that captures the value of x and has its own argument y. When my_func is assigned the result of O_func(2), 
+it is actually the returned i_func with x=2 captured. Finally, my_func(3) calls i_func(3), which returns 2 * 3 = 6."""
