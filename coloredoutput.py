a91f1e596a984f22a49cd90722a42b6da3f06573
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-01 23:30:47 +0530

    Update coloredoutput.py

--- coloredoutput.py
+++ coloredoutput.py
@@ -4,3 +4,10 @@ print(Fore.GREEN + "hello world")
 print(Fore.RED + "hello world")
 print(Fore.YELLOW + "hello world")
 print(Fore.BLUE + "hello world")
+
+
+
+/*This is a Python code that uses the colorama module to print text in different colors in the console.
+The Fore class in the colorama module provides a set of constants that represent different foreground colors.
+The + operator is used to concatenate the color constant with the text to be printed.
+In this code, the text "hello world" is printed four times, each time in a different color: green, red, yellow, and blue.*/
