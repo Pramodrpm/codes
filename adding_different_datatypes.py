bc0df131923790045a4d084908bf46591f806da5
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-03 20:05:15 +0530

    Update adding_different_datatypes.py

--- adding_different_datatypes.py
+++ adding_different_datatypes.py
@@ -1,3 +1,13 @@
 def fun():
     return "little"+' '+"dog"
 print(fun(),sep='')
+
+
+
+
+The code defines a function called fun that takes no arguments and returns the string "little dog".
+
+When the function is called with fun(), the returned string "little dog" is printed to the console because there is no explicit return statement.
+
+The sep='' argument passed to the print function doesn't have any effect in this case because there is only one argument to print.
+The sep parameter specifies the separator character to use between arguments in the print function. Since there is only one argument, no separator is needed.
