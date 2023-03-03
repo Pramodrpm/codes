d5a6c0b8dd1adb8990952fb2e1270bc368cd593e
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-03 20:25:45 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,3 +1,12 @@
 try: print(1)
 except:print(2)
 finally:print(3)
+  
+  
+  
+output is: 1 3
+
+The try block attempts to execute print(1), which will successfully print 1.
+Since there's no error in the try block, the except block is skipped and the code moves on to the finally block.
+The finally block always executes, regardless of whether there was an error or not. So it will print 3.
+Therefore, the output will be 1 followed by 3.
