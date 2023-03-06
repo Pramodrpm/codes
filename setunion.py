15d4351e6139cdd92e63a147b84a9b13160db10b
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-06 22:50:17 +0530

    Update setunion.py

--- setunion.py
+++ setunion.py
@@ -2,3 +2,10 @@ Set1 = {1, 'pramod','is'}
 Set2 = {'2', 'good', 'boy'}
 n = Set1.union(Set2)
 print(n)
+
+
+output:{'od', 1, 'boy', 'good', 'pram', '2'}
+
+
+his code defines two sets Set1 and Set2, and then takes their union using the union method.
+The result is a new set n containing all the elements from both sets.
