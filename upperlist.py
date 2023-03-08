1bdb1b8aa9577117304ade066b69c2dc4efd8144
Pramodrpm <112748309+Pramodrpm@users.noreply.github.com>
2023-03-08 20:05:55 +0530

    Update upperlist.py

--- upperlist.py
+++ upperlist.py
@@ -2,3 +2,9 @@ my_list=["python","coder"]
 for i in my_list:
     my_list.append(i.upper())
 print(my_list)
+
+
+output: infinite loop
+
+This code will result in an infinite loop because the loop is iterating over my_list and appending new elements to it during each iteration.
+Therefore, the length of my_list keeps increasing and the loop never ends.
